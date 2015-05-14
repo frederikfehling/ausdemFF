803156f05d13e3641685fd8ec99492bf1dd0da04
Frederik Fehling <frederik.fehling@hanwis.com>
2015-05-14 16:43:38 +0200

    Configured gulp-livereload in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    livereload = require('gulp-livereload');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('watch', function() {
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any assets, content-files or templates, reload on change
+  gulp.watch(['site/**', 'assets/**', 'content/**']).on('change', livereload.changed);
+
 });
